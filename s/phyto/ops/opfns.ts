a3d28b46c4f5e18b5cb0e29a7fdc3193ba79d198
Chase Moskal <chasemoskal@gmail.com>
2025-04-01 19:23:57 -0700

    wip: phyto ops

--- s/phyto/ops/opfns.ts
+++ s/phyto/ops/opfns.ts
@@ -2,25 +2,69 @@
 import {Vm} from "../../phyto.js"
 import {Opname} from "./opname.js"
 
+function uint(f: number) {
+	if (!Number.isSafeInteger(f))
+		throw new Error("safe integer required")
+	if (f < 0)
+		throw new Error("positive integer required")
+	return f
+}
+
+function truthy(float: number) {
+	return !!float
+}
+
+function bool(bool: boolean) {
+	return bool ? 1 : 0
+}
+
+function getOperandFloat(vm: Vm) {
+	const value = vm.view.getFloat64(vm.position, true)
+	vm.position += 8
+	return value
+}
+
+function getOperandInteger(vm: Vm) {
+	const value = uint(vm.view.getFloat64(vm.position, true))
+	vm.position += 8
+	return value
+}
+
+function getOperandByte(vm: Vm) {
+	const value = vm.view.getUint8(vm.position)
+	vm.position += 1
+	return value
+}
+
+function getOperandBoolean(vm: Vm) {
+	return !!getOperandByte(vm)
+}
+
 export const opfns: Record<keyof typeof Opname, (vm: Vm) => void> = {
 
 	//
 	// outside world stuff
 	//
 
-	capability: _vm => { throw new Error("todo: op not yet implemented") },
-	capcheck: _vm => { throw new Error("todo: op not yet implemented") },
-
 	halt: vm => { vm.done = true },
+	capability: _vm => { throw new Error("todo: op not yet implemented") },
+	capabilitycheck: _vm => { throw new Error("todo: op not yet implemented") },
 
 	//
 	// stack stuff
 	//
 
 	push: vm => {
-		const val = vm.view.getFloat64(vm.position, true)
-		vm.stack.push(val)
-		vm.position += 8
+		const value = getOperandFloat(vm)
+		vm.stack.push(value)
+	},
+
+	pushbytes: vm => {
+		const length = getOperandInteger(vm)
+		for (const byte of vm.bytecode.slice(vm.position, vm.position + length)) {
+			vm.stack.push(byte)
+			vm.position += 1
+		}
 	},
 
 	pop: vm => {
@@ -35,14 +79,38 @@ export const opfns: Record<keyof typeof Opname, (vm: Vm) => void> = {
 	},
 
 	dup: vm => {
-		vm.stack.push(vm.stack.at(vm.stack.size - 1))
+		vm.stack.push(vm.stack.at(-1))
+	},
+
+	dup2: vm => {
+		const a = vm.stack.at(-2)
+		const b = vm.stack.at(-1)
+		vm.stack.push(a)
+		vm.stack.push(b)
+	},
+
+	over: vm => {
+		vm.stack.push(vm.stack.at(-2))
+	},
+
+	select: vm => {
+		const cond = vm.stack.pop()
+		const a = vm.stack.pop()
+		const b = vm.stack.pop()
+		vm.stack.push(cond ? a : b)
 	},
 
 	jump: vm => {
-		const cond = vm.stack.pop()
-		const position = vm.stack.pop()
-		if (cond === 1)
-			vm.position = position
+		const isConditional = getOperandBoolean(vm)
+		const targetPosition = getOperandInteger(vm)
+		if (isConditional) {
+			const condition = vm.stack.pop()
+			if (condition)
+				vm.position = targetPosition
+		}
+		else {
+			vm.position = targetPosition
+		}
 	},
 
 	//
@@ -50,54 +118,155 @@ export const opfns: Record<keyof typeof Opname, (vm: Vm) => void> = {
 	//
 
 	memplz: vm => {
-		const page = vm.memory.memplz()
-		vm.stack.push(page)
+		const pageId = vm.memory.memplz()
+		vm.stack.push(pageId)
 	},
 
 	memcya: vm => {
-		const pageId = vm.stack.pop()
+		const pageId = uint(vm.stack.pop())
 		vm.memory.membye(pageId)
 	},
 
 	memload: vm => {
-		const address = vm.stack.pop()
-		const pageId = vm.stack.pop()
+		const byteMode = getOperandBoolean(vm)
+		const pageId = uint(vm.stack.pop())
+		const address = uint(vm.stack.pop())
+		const length = uint(vm.stack.pop())
 		const page = vm.memory.getPage(pageId)
-		const value = page.view.getFloat64(address, true)
-		vm.stack.push(value)
+		if (byteMode) {
+			for (const byte of page.bytes.slice(address, address + length).reverse())
+				vm.stack.push(byte)
+		}
+		else {
+			const numbers: number[] = []
+			for (let i = 0; i < length; i += 8) {
+				const float = page.view.getFloat64(address + i, true)
+				numbers.push(float)
+			}
+			for (const number of numbers.reverse())
+				vm.stack.push(number)
+		}
 	},
 
 	memstore: vm => {
-		const value = vm.stack.pop()
-		const address = vm.stack.pop()
-		const pageId = vm.stack.pop()
+		const byteMode = getOperandBoolean(vm)
+		const pageId = uint(vm.stack.pop())
+		const address = uint(vm.stack.pop())
+		const length = uint(vm.stack.pop())
 		const page = vm.memory.getPage(pageId)
-		page.view.setFloat64(address, value, true)
+		const numbers = vm.stack.popN(length)
+		if (byteMode) {
+			page.bytes.set(numbers, address)
+		}
+		else {
+			numbers.forEach((number, index) => {
+				page.view.setFloat64(address + (index * 8), number, true)
+			})
+		}
 	},
 
-	// memload_byte: vm => {
-	// 	const address = vm.stack.pop()
-	// 	const pageId = vm.stack.pop()
-	// 	const page = vm.memory.getPage(pageId)
-	// 	const byte = page.bytes.at(address)
-	// 	if (byte === undefined)
-	// 		throw new Error("out of bounds memory access")
-	// 	vm.stack.push(byte)
-	// },
 	//
-	// memstore_byte: vm => {
-	// 	const value = vm.stack.pop()
-	// 	const address = vm.stack.pop()
-	// 	const pageId = vm.stack.pop()
-	// 	const page = vm.memory.getPage(pageId)
-	// 	if (address < 0 || address >= page.bytes.length)
-	// 		throw new Error("out of bounds memory access")
-	// 	page.bytes[address] = value % 256
-	// },
+	// comparison stuff
+	//
+
+	eq: vm => {
+		const b = vm.stack.pop()
+		const a = vm.stack.pop()
+		vm.stack.push(bool(a === b))
+	},
+
+	lt: vm => {
+		const b = vm.stack.pop()
+		const a = vm.stack.pop()
+		vm.stack.push(bool(a < b))
+	},
+
+	lte: vm => {
+		const b = vm.stack.pop()
+		const a = vm.stack.pop()
+		vm.stack.push(bool(a <= b))
+	},
+
+	gt: vm => {
+		const b = vm.stack.pop()
+		const a = vm.stack.pop()
+		vm.stack.push(bool(a > b))
+	},
+
+	gte: vm => {
+		const b = vm.stack.pop()
+		const a = vm.stack.pop()
+		vm.stack.push(bool(a >= b))
+	},
+
+	//
+	// float stuff
+	//
+
+	nan: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(bool(isNaN(a)))
+	},
+
+	finite: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(bool(Number.isFinite(a)))
+	},
+
+	positive: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(bool(a >= 0))
+	},
+
+	integer: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(bool(Number.isSafeInteger(a)))
+	},
+
+	//
+	// boolean stuff
+	//
+
+	not: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(bool(!truthy(a)))
+	},
+
+	truthy: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(bool(truthy(a)))
+	},
+
+	falsy: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(bool(!truthy(a)))
+	},
 
 	//
 	// math stuff
 	//
+
+	abs: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(Math.abs(a))
+	},
+
+	sign: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(Math.sign(a))
+	},
+
+	min: vm => {
+		const b = vm.stack.pop()
+		const a = vm.stack.pop()
+		vm.stack.push(Math.min(a, b))
+	},
+
+	max: vm => {
+		const b = vm.stack.pop()
+		const a = vm.stack.pop()
+		vm.stack.push(Math.max(a, b))
+	},
 
 	add: vm => {
 		const b = vm.stack.pop()
@@ -149,9 +318,19 @@ export const opfns: Record<keyof typeof Opname, (vm: Vm) => void> = {
 		vm.stack.push(Math.trunc(a))
 	},
 
+	round: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(Math.round(a))
+	},
+
 	sqrt: vm => {
 		const a = vm.stack.pop()
 		vm.stack.push(Math.sqrt(a))
+	},
+
+	cbrt: vm => {
+		const a = vm.stack.pop()
+		vm.stack.push(Math.cbrt(a))
 	},
 }
 
--- s/phyto/ops/opname.ts
+++ s/phyto/ops/opname.ts
@@ -4,39 +4,36 @@ export enum Opname {
 
 	// outside world stuff
 	halt,
-	capability, // [u64 length], ...[u8 name] ...(any)
-	capcheck, // [u64 length], ...[u8 name] {pushes 1 if capability exists}
+	capability, // [f64 length], ...[u8 name] ...(any)
+	capabilitycheck, // [f64 length], ...[u8 name] {pushes 1 if capability exists}
 
 	// stack stuff
-	push, // [f64] -> (number)
-	// pushbytes, // [u64 length], ...[u8 bytes] {adds bytes to stack as f64s}
-	pop,
-	swap,
-	dup,
-	// dup2,
-	// over,
-	// select, // (cond) (a) (b) {pushes a to stack if cond is 1}
-	jump, // [bool cond], [u64 position] {cond true will pop jump condition from stack}
+	push, // [f64] {add a literal f64 to stack}
+	pushbytes, // [f64 length], ...[u8 bytes] {add bytes to stack as f64s}
+	pop, swap, dup, dup2, over,
+	select, // (cond) (a) (b) {pushes a to stack if cond is 1}
+	jump, // [bool is-conditional], [f64 position] {cond true will pop jump condition from stack}
 
 	// memory stuff
 	memplz, // {request memory page, page-id pushed to stack}
 	memcya, // (page-id) {free a memory page}
 	memload, // [bool byte-mode], (page-id), (address), (length)
-	memstore, // [bool byte-mode], [u64 length], (page-id), (address), ...(data)
+	memstore, // [bool byte-mode], (page-id), (address), (length), ...(data)
 
-	// // comparison stuff
-	// eq, lt, gt, lte, gte,
-	//
-	// // bitwise stuff
-	// band, bor, bxor, bnot, bshiftleft, bshiftright,
+	// comparison stuff
+	eq, lt, gt, lte, gte,
 
-	// // math stuff
-	// abs, sign, min, max,
+	// float stuff
+	nan, finite, positive, integer,
+
+	// booleans
+	not, truthy, falsy,
+
+	// math stuff
+	abs, sign, min, max,
 	add, sub, mul, div, rem, neg,
-	floor, ceil, trunc,
-	// round,
-	sqrt,
-	// cbrt,
+	floor, ceil, trunc, round,
+	sqrt, cbrt,
 	// pow, exp, log, log10,
 
 	// // trig stuff
